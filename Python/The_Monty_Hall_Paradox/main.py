2cb0e365b0c0083b6277c9824150a5a8ede64d94
Admin <alasvlaoscsvsudktas@gmail.com>
2022-11-20 07:48:30 +0600

    asdfasfafasdfsadf

--- Python/The_Monty_Hall_Paradox/main.py
+++ Python/The_Monty_Hall_Paradox/main.py
@@ -3,7 +3,7 @@ from random import randrange
 def increase_percents(was, became):
     return became * 100 / was - 100
 
-number_of_doors = 3
+number_of_doors = 70
 probability_of_winning = 1 / number_of_doors
 
 available_doors = [i + 1 for i in range(number_of_doors)]
@@ -13,19 +13,19 @@ player_number = int(input("Выбирите одну дверь двери из 
 
 available_doors.remove(player_number)
 
-show_host_number = randrange(1, 3)
+show_host_number = randrange(1, number_of_doors)
 while show_host_number == player_number:
-    show_host_number = randrange(1, 3)
+    show_host_number = randrange(1, number_of_doors)
 
 available_doors.remove(show_host_number)
 
 print("Ведущий игры выбрал дверь под номером: ", show_host_number)
 
-change_the_choise = input(f"Хотите ли вы выбрать дверь под номером {available_doors.pop()} (да/нет): ")
+change_the_choise = input(f"Хотите ли вы выбрать другую дверь (да/нет): ")
 
 if change_the_choise == "да":
     print("Так как вы выбрали другую дверь, то вероятность того, что приз находится за этой "
-          "дверью выше: ", increase_percents(probability_of_winning, 0.5))
+          "дверью выше: ", increase_percents(probability_of_winning, 1 / len(available_doors)))
 
 if change_the_choise == "нет":
     print("Вероятность того, что приз за дверью, которую вы выбрали в первый раз "
