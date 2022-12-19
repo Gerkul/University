8cb3673b09230cbb7c03c23d0d1762a2e3c923ad
Admin <alasvlaoscsvsudktas@gmail.com>
2022-12-19 23:08:38 +0600

    asdfasfafasdfsadf

--- Python/practice4/main.py
+++ Python/practice4/main.py
@@ -1,29 +1,31 @@
-
 import re
+from typing import List, Any
+
+
 def read_file(file_name):
-    words = []
+    words= []
 
     with open(file_name, 'r', encoding="UTF-8") as file:
         for line in file.readlines():
             line = re.findall('[a-zа-яё]+', line, flags=re.IGNORECASE)
             for word in line:
-                if word in words:
+                if word.lower() in words:
                     continue
                 else:
-                    words.append(word)
+                    words.append(word.lower())
 
     return words
 
 
 def save_file(file_name, words):
     with open(file_name, 'w', encoding="UTF-8") as file:
+        file.write("Всего уникалных слов: " + str(len(words)) + '\n')
+        file.write("======================" + '\n')
         for word in words:
             file.write(word + '\n')
-        file.write(str(len(words)) + '\n')
 
 
-words = read_file("text.txt")
+words = read_file("data.txt")
 words.sort()
-print(words)
 
 save_file("words.txt", words)
